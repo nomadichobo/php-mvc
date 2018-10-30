e8d001d23317b038a9bacb9ab499ac34ba7e91f3
Martin Svensson <nomadichobo.com@gmail.com>
2018-10-30 17:19:59 +0100

    Heade and Footer

--- app/config/config.php
+++ app/config/config.php
@@ -2,6 +2,6 @@
     // App Root
     define('APPROOT', dirname(dirname(__FILE__)));
     // URL Root
-    define('URLROOT', 'http://localhost/projects/PHP/php-mvc');
+    define('URLROOT', 'http://localhost/projects/PHP/php-mvc/');
     // Site Name
     define('SITENAME', 'PHP-MVC');
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -13,6 +13,10 @@
         }
 
         public function about() {
-            $this->view('pages/about');
+            $data = 
+                [
+                    'title' => 'About'
+                ];
+            $this->view('pages/about', $data);
         }
     }
